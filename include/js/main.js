fa1fa4dd70686a3253b50348b6973401a01441d5
SergeiVictorovich <md.men@ya.ru>
2016-10-07 18:33:22 +0300

    index 4

--- include/js/main.js
+++ include/js/main.js
@@ -14,16 +14,16 @@ $(document).ready(function(){
 
         switch(action) {
             case 'main':
-                location.href = '/curs-3d/';
+                location.href = '/';
                 return;
             case 'report':
-                frame.attr('src', '/curs-3d/_include/_docs/work.pdf');
+                frame.attr('src', 'include/docs/work.pdf');
                 break;
             case 'scene_0':
-                frame.attr('src', '/curs-3d/_include/_webplayer/webplayer.html?load=/curs-3d/_include/_scenes/_first/curs.json&no_social&autorotate');
+                frame.attr('src', 'include/webplayer/webplayer.html?load=/include/scenes/first/curs.json&no_social&autorotate');
                 break;
             case 'scene_1':
-                frame.attr('src', '/curs-3d/_include/_webplayer/webplayer.html?load=/curs-3d/_include/_scenes/_final/final.json&no_social&autorotate');
+                frame.attr('src', 'include/webplayer/webplayer.html?load=/include/scenes/final/final.json&no_social&autorotate');
                 break;
             default:
                 error = true;
