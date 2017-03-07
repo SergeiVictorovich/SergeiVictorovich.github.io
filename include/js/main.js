402cef6cd7b1d2173d381ef47542c1dad51d7746
Dmitry Menovschikov <dmitry.corsa@gmail.com>
2017-03-08 00:05:49 +0300

    Пути к сценам в js файле, мелкие косметические правки index.html

--- include/js/main.js
+++ include/js/main.js
@@ -18,16 +18,16 @@ $(document).ready(function () {
                 location.href = '/';
                 return;
             case 'report':
-                frame.attr('src', 'include/docs/curs.pdf');
+                frame.attr('src', 'cp/docs/curs.pdf');
                 break;
             case 'scene_0':
-                frame.attr('src', 'include/webplayer/webplayer.html?load=/include/scenes/first/curs.json&no_social&autorotate');
+                frame.attr('src', 'cp/webplayer/webplayer.html?load=/cp/scenes/first/curs.json&no_social&autorotate');
                 break;
             case 'scene_1':
-                frame.attr('src', 'include/webplayer/webplayer.html?load=/include/scenes/final/final.json&no_social&autorotate');
+                frame.attr('src', 'cp/webplayer/webplayer.html?load=/cp/scenes/final/final.json&no_social&autorotate');
                 break;
             case 'screensaver':
-                frame.attr('src', 'include/scenes/screensaver/index.html');
+                frame.attr('src', 'cp/scenes/screensaver/index.html');
                 break;
             case 'awesome_0':
                 frame.attr('src', 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/capri/props/watch_scene/watch_scene.json&autorotate&no_social&compressed_textures&compressed_textures_pvr');
@@ -39,13 +39,13 @@ $(document).ready(function () {
                 frame.attr('src', 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/interactivity/solar_system/solar_system_ru.json&compressed_textures&compressed_textures_pvr&no_social');
                 break;
             case 'awesome_2':
-                frame.attr('src', 'include/scenes/awesome/2/index.html');
+                frame.attr('src', 'cp/scenes/awesome/2/index.html');
                 break;
             case 'awesome_3':
-                frame.attr('src', 'include/scenes/awesome/3/index.html');
+                frame.attr('src', 'cp/scenes/awesome/3/index.html');
                 break;
             case 'awesome_4':
-                frame.attr('src', 'include/scenes/awesome/4/index.html');
+                frame.attr('src', 'cp/scenes/awesome/4/index.html');
                 break;
             default:
                 error = true;
