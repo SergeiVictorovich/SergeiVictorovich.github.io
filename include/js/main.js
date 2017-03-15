1db5d4f5f50eadcf2c40ed1a1cba05ac75c8c34a
md <dmitry.corsa@gmail.com>
2017-03-15 19:35:44 +0300

    Внесен ряд правок

--- include/js/main.js
+++ include/js/main.js
@@ -1,94 +1,114 @@
-'use strict';
+;(function () {
 
-$(document).ready(function () {
+    'use strict';
 
-    var $container = $('.container-fluid');
+    var container = document.getElementsByClassName('container-fluid')[0],
+        bg = document.getElementById('bg'),
+        frame = document.getElementById('frame');
 
-    $('#bg').addClass('visible');
+    bg.className += ' visible';
 
-    $('.js-toggle').on('click', function () {
+    [].forEach.call(document.getElementsByClassName('js-toggle'), function (elem) {
 
-        var action = $(this).data('action');
-        var frame = $('#frame');
-        var video = $('#bg');
-        var error = false;
+        elem.addEventListener('click', function () {
 
-        switch (action) {
-            case 'main':
-                location.href = '/';
-                return;
-            case 'report':
-                frame.attr('src', 'include/docs/curs.pdf');
-                break;
-            case 'scene_0':
-                frame.attr('src', 'include/webplayer/webplayer.html?load=/cp/include/scenes/first/curs.json&no_social&autorotate');
-                break;
-            case 'scene_1':
-                frame.attr('src', 'include/webplayer/webplayer.html?load=/cp/include/scenes/final/final.json&no_social&autorotate');
-                break;
-            case 'awesome_0':
-                frame.attr('src', 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/capri/props/watch_scene/watch_scene.json&autorotate&no_social&compressed_textures&compressed_textures_pvr');
-                break;
-            case 'awesome_1':
-                frame.attr('src', 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/capri/buildings/fountain_elephants/fountain_elephants.json&autorotate&compressed_textures&compressed_textures_pvr&no_social');
-                break;
-            case 'awesome_1.5':
-                frame.attr('src', 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/interactivity/solar_system/solar_system_ru.json&compressed_textures&compressed_textures_pvr&no_social');
-                break;
-            case 'awesome_2':
-                frame.attr('src', 'include/scenes/awesome/2/index.html');
-                break;
-            case 'awesome_3':
-                frame.attr('src', 'include/scenes/awesome/3/index.html');
-                break;
-            case 'awesome_4':
-                frame.attr('src', 'include/scenes/awesome/4/index.html');
-                break;
-            default:
-                error = true;
-        }
+            var action = this.dataset.action;
 
-        if (!error) {
-            video.remove();
-        } else {
-            alert('Документ не найден!');
-        }
+            switch (action) {
+                case 'main':
+                    location.href = '/';
+                    return;
+                case 'report':
+                    frame.src = 'include/docs/curs.pdf';
+                    break;
+                case 'scene_0':
+                    frame.src = 'include/webplayer/webplayer.html?load=/cp/include/scenes/first/curs.json&no_social&autorotate';
+                    break;
+                case 'scene_1':
+                    frame.src = 'include/webplayer/webplayer.html?load=/cp/include/scenes/final/final.json&no_social&autorotate';
+                    break;
+                case 'awesome_0':
+                    frame.src = 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/capri/props/watch_scene/watch_scene.json&autorotate&no_social&compressed_textures&compressed_textures_pvr';
+                    break;
+                case 'awesome_1':
+                    frame.src = 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/capri/buildings/fountain_elephants/fountain_elephants.json&autorotate&compressed_textures&compressed_textures_pvr&no_social';
+                    break;
+                case 'awesome_1.5':
+                    frame.src = 'https://www.blend4web.com/apps/webplayer/webplayer.html?load=../../assets/interactivity/solar_system/solar_system_ru.json&compressed_textures&compressed_textures_pvr&no_social';
+                    break;
+                case 'awesome_2':
+                    frame.src = 'include/scenes/awesome/2/index.html';
+                    break;
+                case 'awesome_3':
+                    frame.src = 'include/scenes/awesome/3/index.html';
+                    break;
+                case 'awesome_4':
+                    frame.src = 'include/scenes/awesome/4/index.html';
+                    break;
+                default:
+                    alert('Документ не найден!');
+                    return;
+            }
 
-    });
+            bg.remove();
 
-    var $info = $container.find('.info-wrap');
-    var infoWidth = $info.width();
-    var infoHeight = $info.height();
-
-    $container.on('click', '.info-wrap-hide', function () {
-
-        var $self = $(this);
-
-        if ($self.hasClass('active')) {
-            $info
-                .width(infoWidth)
-                .height(infoHeight);
-        } else {
-            $info
-                .width(10)
-                .height(10);
-        }
-
-        $info.toggleClass('_hidden');
-        $self.toggleClass('active');
-
-        if (!$info.hasClass('_hidden')) {
-
-            setTimeout(function () {
-                $info.css('height', 'auto');
-            }, 300);
-
-        }
-
-    }).on('click', '.js-awesome, .resources-btn', function () {
-
-        $(this).next().slideToggle(300);
+        });
 
     });
 
-});
+    var info = document.getElementsByClassName('info-wrap')[0],
+        width = null,
+        height = null;
+    
+    document.getElementsByClassName('info-wrap-hide')[0].addEventListener('click', function () {
+
+        info.style.width = info.clientWidth + 'px';
+        info.style.height = info.clientHeight + 'px';
+
+        if (!info.classList.contains('_hidden')) {
+            width = info.clientWidth;
+            height = info.clientHeight;
+        } else {
+            info.style.width = width + 'px';
+            info.style.height = height + 'px';
+        }
+
+        toggleClass(info, '_hidden');
+        toggleClass(this, 'active');
+
+    });
+
+    [].forEach.call(document.getElementsByClassName('js-slide'), function (elem) {
+
+        var slideBlock = elem.nextElementSibling;
+        if (slideBlock.nodeType != 1) {
+            slideBlock = slideBlock.nextElementSibling;
+        }
+
+        elem.addEventListener('click', function () {
+
+            info.style.height = 'auto';
+
+            toggleClass(slideBlock, 'visible');
+
+        });
+
+    });
+
+    function toggleClass(el, className) {
+        if (el.classList) {
+            el.classList.toggle(className);
+        } else {
+            var classes = el.className.split(' ');
+            var existingIndex = classes.indexOf(className);
+
+            if (existingIndex >= 0)
+                classes.splice(existingIndex, 1);
+            else
+                classes.push(className);
+
+            el.className = classes.join(' ');
+        }
+    }
+
+})();
